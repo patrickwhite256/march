096c0f50ded96f80691479c63f5ed1403cb38747
Joel Mizzoni <jpmizzoni@gmail.com>
2015-10-04 01:08:21 -0400

    LOOK I MADE SOME LINES. Also a slider thing and fixed up the constructors to be more python 3 like

--- march.py
+++ march.py
@@ -7,6 +7,9 @@ if __name__ == '__main__':
 	songFile = None
 	if(len(sys.argv) > 1):
 		songFile = open(sys.argv[1], 'r')
+	else:
+		print("I'm temporarily requiring you to pass an .sm file as an argument right now")
+		exit()
 	wnd = MarchWindow(songFile)
 	wnd.show()
 	app.exec_()
