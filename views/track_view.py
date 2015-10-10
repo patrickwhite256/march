71d18d8d8b55a6203cf044fe9069a1b6ddc7a2e3
Kelly McBride <kemcbride28@gmail.com>
2015-10-10 16:16:51 -0400

    Rendering arrows more "rhythmically correctly" or something

--- views/track_view.py
+++ views/track_view.py
@@ -1,135 +1,180 @@
 from PyQt5.QtWidgets import QWidget, QFrame, QHBoxLayout, QVBoxLayout, QSizePolicy, QGridLayout, QPushButton, QSlider
 from PyQt5.QtGui import QPainter, QPen, QBrush, QPixmap
 from PyQt5.QtCore import Qt, QRect
-from models import Chart, Note
-from .arrow_label import MarchArrowLabel
+from models import Chart, Note, Tap
+from .arrow_label import MarchArrowLabel, ARROW_SIZE
 
+# space between vertical "intervals" in px
 INTERVAL_SPACING = 40
+# space between horizontal columns/arrows in px
 ARROW_SPACING = 80
 
 class MarchTrackView(QWidget):
 
-	column = -1
-	row = -1
+    column = -1
+    row = -1
 
-	def __init__(self, parent, model):
-		super().__init__(parent)
-		
-		# Properties
-		self.interval = 4
-		self.top_offset = 50 + ARROW_SPACING
+    def __init__(self, parent, model):
+        super().__init__(parent)
 
-		self.setModel(model)
-		self.initUI()
-		self.setMouseTracking(True)
+        # Properties
+        self.interval = 4
+        self.top_offset = 50 + ARROW_SPACING
 
-	def initUI(self):
-		self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.setModel(model)
+        self.initUI()
+        self.setMouseTracking(True)
 
-		scale = QSlider(Qt.Vertical, self)
-		scale.valueChanged.connect(self.intervalChangeEvent)
-		scale.setMinimum(4)
-		scale.setMaximum(192)
-		scale.setTickInterval(4)
-		scale.setSingleStep(4)
+    def initUI(self):
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
-		leftArrow = MarchArrowLabel(Note.POSITION_LEFT, self)
-		leftArrow.move(90, 50)
-		leftArrow.show()
-		downArrow = MarchArrowLabel(Note.POSITION_DOWN, self)
-		downArrow.move(90 + ARROW_SPACING, 50)
-		downArrow.show()
-		upArrow = MarchArrowLabel (Note.POSITION_UP, self)
-		upArrow.move(90 + ARROW_SPACING * 2, 50)
-		upArrow.show()
-		rightArrow = MarchArrowLabel(Note.POSITION_RIGHT, self)
-		rightArrow.move(90 + ARROW_SPACING * 3, 50)
-		rightArrow.show()
+        scale = QSlider(Qt.Vertical, self)
+        scale.valueChanged.connect(self.intervalChangeEvent)
+        scale.setMinimum(4)
+        scale.setMaximum(192)
+        scale.setTickInterval(4)
+        scale.setSingleStep(4)
 
-		self.layout = QVBoxLayout()
-		self.layout.addWidget(scale)
+        leftArrow = MarchArrowLabel(Note.POSITION_LEFT, self)
+        leftArrow.move(90, 50)
+        leftArrow.show()
+        downArrow = MarchArrowLabel(Note.POSITION_DOWN, self)
+        downArrow.move(90 + ARROW_SPACING, 50)
+        downArrow.show()
+        upArrow = MarchArrowLabel(Note.POSITION_UP, self)
+        upArrow.move(90 + ARROW_SPACING * 2, 50)
+        upArrow.show()
+        rightArrow = MarchArrowLabel(Note.POSITION_RIGHT, self)
+        rightArrow.move(90 + ARROW_SPACING * 3, 50)
+        rightArrow.show()
 
-		self.setLayout(self.layout)
-		self.updateGeometry()
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(scale)
 
-	def setModel(self, chart):
-		'''
-		Set the model of the track. 
-		The model should be a Chart object.
-		'''
-		self.model = chart
+        self.setLayout(self.layout)
+        self.updateGeometry()
 
-	def drawChart(self, qp):
+    def setModel(self, chart):
+        '''
+        Set the model of the track.
+        The model should be a Chart object.
+        '''
+        self.model = chart
 
-		brush = QBrush(Qt.red, Qt.SolidPattern)
-		qp.setBrush(brush)
+    def drawChart(self, qp):
 
-		qpen = QPen()
+        brush = QBrush(Qt.red, Qt.SolidPattern)
+        qp.setBrush(brush)
 
-		for measure_index, measure in enumerate(self.model.measures):
+        qpen = QPen()
 
-			for beat_index in range(self.interval):
+        for measure_index, measure in enumerate(self.model.measures):
 
-				current_beat = measure_index * self.interval + beat_index	
+                for beat_index in range(self.interval):
 
-				if (beat_index == 0):
-					qpen.setStyle(Qt.SolidLine)
-					qpen.setWidth(2)
-				else:
-					qpen.setStyle(Qt.DashLine)
-					qpen.setWidth(1)
+                    current_beat = measure_index * self.interval + beat_index
 
-				if (current_beat == self.row):
-					qpen.setColor(Qt.red)
-					qpen.setWidth(qpen.width() * 2)
-				else:
-					qpen.setColor(Qt.black)
+                    if (beat_index == 0):
+                        qpen.setStyle(Qt.SolidLine)
+                        qpen.setWidth(2)
+                    else:
+                        qpen.setStyle(Qt.DashLine)
+                        qpen.setWidth(1)
 
-				qp.setPen(qpen)
-			
-				y = current_beat * INTERVAL_SPACING
-				qp.drawLine(100, y, 400, y)
-			
-		
-		if(self.column > -1):
-			columnStart = 100 + ARROW_SPACING * self.column
-			columnEnd = 100 + ARROW_SPACING * (self.column + 1)
+                    if (current_beat == self.row):
+                        qpen.setColor(Qt.red)
+                        qpen.setWidth(qpen.width() * 2)
+                    else:
+                        qpen.setColor(Qt.black)
 
-			qp.drawLine(columnStart, 0, columnStart, self.height())
-			qp.drawLine(columnEnd, 0, columnEnd, self.height())
+                    qp.setPen(qpen)
 
+                    y = current_beat * INTERVAL_SPACING + self.top_offset
+                    qp.drawLine(100, y, 400, y)
 
-	def drawNotes(self, qp):
-		for measure_index, measure in enumerate(self.model.measures):
-			note_interval = len(measure.notes) / self.interval
-			measure_offset = INTERVAL_SPACING * self.interval * measure_index
+        if(self.column > -1):
+            columnStart = 100 + ARROW_SPACING * self.column
+            columnEnd = 100 + ARROW_SPACING * (self.column + 1)
 
-			for note in measure.notes:
-				arrow = MarchArrowLabel(note.position)
-				note_voffset = note.offset * note_interval
-				note_hoffset = 100 + ARROW_SPACING * note.postion # hurray for enums
+            qp.drawLine(columnStart, 0, columnStart, self.height())
+            qp.drawLine(columnEnd, 0, columnEnd, self.height())
 
+    def drawNotes(self, qp):
+        for measure_index, measure in enumerate(self.model.measures):
+            # render only first 3 measures. let's figure this shit out.
+            # TODO/TESTING
+            # reader's note: testing with now_or_never.sm, only measure 2 has notes
+            # if measure_index == 2:
+            # print("measure index", measure_index)
+            note_interval = INTERVAL_SPACING / (measure.rows / self.interval)
+            measure_offset = INTERVAL_SPACING * self.interval * measure_index + self.top_offset
+            # print("measure_offset", measure_offset)
+            # print("note interval", note_interval)
 
-	# Event Handlers
+            for note in measure.notes:
+                arrow = MarchArrowLabel(note.position, self)
+                note_voffset = measure_offset + note.offset * note_interval - ARROW_SIZE / 2
+                # TODO/TESTING here is where i am printing for now
+                # print(note.offset, note_voffset)
+                note_hoffset = 90 + ARROW_SPACING * note.position  # hurray for enums
 
-	def paintEvent(self, event):
-		# TODO Optimize to only draw the visble portion of the chart
-		qp = QPainter()
-		qp.begin(self)
-		self.drawChart(qp)
-		self.drawNotes(qp)
-		qp.end()
+                source_rect = QRect(0, 0, ARROW_SPACING, ARROW_SPACING)
+                target_rect = QRect(note_hoffset, note_voffset, ARROW_SPACING, ARROW_SPACING)
+                qp.drawPixmap(target_rect, arrow.pixmap(), source_rect)
 
-	def intervalChangeEvent(self, data):
-		self.interval = data
-		print(data)
-		self.update()
+    # Event Handlers
 
-	def mouseMoveEvent(self, event):
-		x = event.pos().x()
-		y = event.pos().y()
+    def paintEvent(self, event):
+        # TODO Optimize to only draw the visble portion of the chart
+        qp = QPainter()
+        qp.begin(self)
+        self.drawChart(qp)
+        self.drawNotes(qp)
+        qp.end()
 
-		self.column = min(int((x - 100) / ARROW_SPACING), 3)
-		self.row = int(y / INTERVAL_SPACING)
+    def intervalChangeEvent(self, data):
+        self.interval = data
+        print(data)
+        # KELLY TESTING TODO UNCOMMENT
+        self.update()
 
-		self.update()
+    def mouseMoveEvent(self, event):
+        x = event.pos().x()
+        y = event.pos().y()
+
+        self.column = min(int((x - 100) / ARROW_SPACING), 3)
+        self.row = int((y - self.top_offset) / INTERVAL_SPACING)
+
+        # KELLY TESTING TODO UNCOMMENT
+        self.update()
+
+    def mousePressEvent(self, event):
+        measure_index = int(self.row / self.interval)
+        row_offset = self.row % self.interval
+        print(row_offset)
+        selected_measure = self.model.measures[measure_index]
+
+        # TODO Put note existence search fn in model
+        init_len = len(selected_measure.notes)
+
+        selected_measure.notes[:] = [note for note in selected_measure.notes if (note.offset != row_offset or note.position != self.column)]
+
+        if (init_len == len(selected_measure.notes)):
+            # didn't remove anything, add the note here
+            new_note = Tap()
+            new_note.position = self.column
+            new_note.offset = row_offset
+            selected_measure.notes.append(new_note)
+
+        # KELLY TESTING TODO UNCOMMENT
+        self.update()
+
+    def wheelEvent(self, event):
+        if (event.angleDelta() is not None):
+            if (event.angleDelta().y() > 0):
+                self.top_offset += 15
+            else:
+                self.top_offset -= 15
+
+        # KELLY TESTING TODO UNCOMMENT
+        self.update()
